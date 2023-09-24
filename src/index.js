e378ea02ffdc3fe1dedd1b92fe7b2215a595b492
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-24 11:30:15 +0330

    make detail page and describe crops

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { StrictMode } from 'react';
+import { BrowserRouter } from "react-router-dom"
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>
 );
